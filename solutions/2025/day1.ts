25dafb597a66f1132e6cd9f933975ae09c171ecd
Noah Geren <noah.s.geren@gmail.com>
2025-11-30 23:46:26 -0600

    Solved day 1

---
+++ solutions/2025/day1.ts
@@ -0,0 +1,27 @@
+import { readFileSync } from "node:fs";
+
+let data = readFileSync("./data/2025/day1.txt")
+	.toString()
+	.split("\n")
+	.map((row) => ({
+		direction: row.charAt(0) === "R" ? 1 : -1,
+		value: +row.substring(1)
+	}));
+
+let dial = 50;
+let zeroStopCount = 0;
+let zeroMoveCount = 0;
+for (const rotation of data) {
+	for (let i = 0; i < rotation.value; i++) {
+		dial = (dial + rotation.direction + 100) % 100;
+		if (!dial) {
+			zeroMoveCount++;
+		}
+	}
+	if (!dial) {
+		zeroStopCount++;
+	}
+}
+
+console.log("Part 1: ", zeroStopCount);
+console.log("Part 2: ", zeroMoveCount);
