3ab196fe9a3a22b3b11b6081ea884a055d4ab41f
Noah Geren <noah.s.geren@gmail.com>
2025-12-02 20:56:44 -0600

    Combined both parts of 2025 day 2

--- solutions/2025/day2.ts
+++ solutions/2025/day2.ts
@@ -15,29 +15,6 @@ const ranges = readFileSync("./data/2025/day2.txt")
 const repeatPrefix = (prefix: number, count = 2): BigNumber => {
 	return BigNumber(`${prefix}`.repeat(count));
 };
-const getStartingPrefix = (start: BigNumber): number => {
-	const length = start.toString().length;
-	if (length % 2 === 0) {
-		const prefix = +start.toString().substring(0, length / 2);
-		if (repeatPrefix(prefix).comparedTo(start) < 0) {
-			return prefix + 1;
-		}
-		return prefix;
-	}
-	return Math.pow(10, Math.floor(length / 2));
-};
-const partOneAnswer = ranges.reduce<BigNumber>((currentSum, [start, end]) => {
-	for (
-		let prefix = getStartingPrefix(start);
-		repeatPrefix(prefix).comparedTo(end) <= 0;
-		prefix++
-	) {
-		currentSum = currentSum.plus(repeatPrefix(prefix));
-	}
-	return currentSum;
-}, BigNumber(0));
-
-console.log("Part 1:", partOneAnswer);
 
 const maxValue = ranges
 	.map((range) => range[1])
@@ -51,6 +28,7 @@ const maxCount = maxValue.toString().length;
 const maxPrefix = +maxValue.toString().substring(0, Math.floor(maxCount / 2));
 const seenPrefixes: Record<number, boolean> = {};
 
+let partOneAnswer = BigNumber(0);
 let partTwoAnswer = BigNumber(0);
 for (let prefix = 1; prefix <= maxPrefix; prefix++) {
 	if (seenPrefixes[prefix]) {
@@ -71,9 +49,13 @@ for (let prefix = 1; prefix <= maxPrefix; prefix++) {
 					invalidId.comparedTo(end) <= 0
 			)
 		) {
+			if (count % 2 === 0) {
+				partOneAnswer = partOneAnswer.plus(invalidId);
+			}
 			partTwoAnswer = partTwoAnswer.plus(invalidId);
 		}
 	}
 }
 
+console.log("Part 1:", partOneAnswer);
 console.log("Part 2:", partTwoAnswer);
