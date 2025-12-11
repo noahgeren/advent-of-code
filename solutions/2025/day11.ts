61506b718e9eccc46cbab0ac4d2591d64bd65ac2
Noah Geren <noah.s.geren@gmail.com>
2025-12-11 11:57:04 -0600

    Solved day 11 of 2025

---
+++ solutions/2025/day11.ts
@@ -0,0 +1,39 @@
+import HashMap from "#/utilities/structures/HashMap";
+import { readFileSync } from "node:fs";
+
+const connections = readFileSync("./data/2025/day11.txt")
+	.toString()
+	.split("\n")
+	.reduce<Record<string, string[]>>((connections, row) => {
+		const tokens = row.split(" ");
+		connections[tokens[0].replaceAll(":", "")] = tokens.slice(1);
+		return connections;
+	}, {});
+
+const seenPaths = new HashMap<string[], number>((key) => key.join());
+const countPaths = (device: string, needed: string[] = []): number => {
+	if (device === "out") {
+		return needed.length === 0 ? 1 : 0;
+	}
+	const key = [device, ...needed];
+	if (seenPaths.has(key)) {
+		return seenPaths.get(key)!;
+	}
+	const paths = connections[device].reduce<number>(
+		(paths, newDevice) =>
+			paths +
+			countPaths(
+				newDevice,
+				needed.filter((d) => d !== device)
+			),
+		0
+	);
+	seenPaths.set(key, paths);
+	return paths;
+};
+
+console.log("Part 1:", countPaths("you"));
+
+seenPaths.clear();
+
+console.log("Path 2:", countPaths("svr", ["dac", "fft"]));
