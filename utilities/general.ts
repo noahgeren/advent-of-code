0b0a023240bb36be2b49b69851965e8039532370
Noah Geren <noahsgeren@gmail.com>
2025-12-29 13:02:33 -0600

    Added utilities for GCD and LCM

--- utilities/general.ts
+++ utilities/general.ts
@@ -46,6 +46,37 @@ export const intersection = <T>(xs?: T[], ys?: T[], ...rest: T[][]): T[] =>
 export const readInputFile = (year: number, day: number) =>
 	readFileSync(`./data/${year}/day${day}.txt`).toString();
 
+export const gcd = (...values: (number | BigNumber)[]): BigNumber => {
+	if (values.length === 1) {
+		return BigNumber(values[0]);
+	}
+	if (values.length === 2) {
+		if (BigNumber(values[1]).isZero()) {
+			return BigNumber(values[0]);
+		}
+		return gcd(values[1], BigNumber(values[0]).mod(values[1]));
+	}
+	return values.reduce<BigNumber>(
+		(currentGcd, value) => gcd(currentGcd, value),
+		BigNumber(values[0])
+	);
+};
+
+export const lcm = (...values: (number | BigNumber)[]): BigNumber => {
+	if (values.length === 1) {
+		return BigNumber(values[0]);
+	}
+	if (values.length === 2) {
+		return BigNumber(values[0])
+			.times(values[1])
+			.div(gcd(values[0], values[1]));
+	}
+	return values.reduce<BigNumber>(
+		(currentLcm, value) => lcm(currentLcm, value),
+		BigNumber(1)
+	);
+};
+
 // TODO:
 //  - Helper function to sum values in array
 //  - Update number based helpers to work with BigNumber
